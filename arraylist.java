ae2df17553048206d2f87bc2aa26bd5b70f33ddf
venuchalla <venuchalla1993@gmail.com>
2017-04-08 23:56:02 -0400

    Create arraylist.java

---
+++ arraylist.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class ArrayListDemo {
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> arrayList=new ArrayList<String>();
+		arrayList.add("Venu");
+		arrayList.add("vardhan");
+		arrayList.add("challa");
+		arrayList.add("reddy");
+		System.out.println(arrayList);
+		
+	}
+
+}
+
